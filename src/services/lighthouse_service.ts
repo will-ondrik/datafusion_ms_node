d9b1017fee25a741900dc17d4d7a6966596a7d8e
Will Ondrik <wondrik@my.bcit.ca>
2025-01-15 05:30:12 -0800

    Removed logs and unused fs import.

--- src/services/lighthouse_service.ts
+++ src/services/lighthouse_service.ts
@@ -1,17 +1,13 @@
 
 import lighthouse, { RunnerResult } from 'lighthouse';
-import fs from 'fs';
 import * as chromeLauncher from 'chrome-launcher';
-
 
 class LighthouseService {
     constructor() {
-        console.log('initializing lighthouse service')
         this.runAudit = this.runAudit.bind(this);
     }
 
     async runAudit(url: string): Promise<RunnerResult | undefined> {
-        console.log('Running audit for url:', url);
         const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless']});
         const options = {
             logLevel: 'info' as 'info', 
@@ -23,16 +19,12 @@ class LighthouseService {
             ],
             port: chrome.port,
         };
-
         const runnerResult: RunnerResult | undefined = await lighthouse(url, options);
         chrome.kill();
         
         console.log('runnerResult', runnerResult);
         return runnerResult;
-        
     }
-
-
 }
 
 export default LighthouseService;
